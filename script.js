4299ccc8c2c8527a4e3b46642df7243d0f57ac3e
v4nessa04 <v4nessa04@gmail.com>
2023-09-06 17:56:34 -0400

    changes made

--- script.js
+++ script.js
@@ -32,6 +32,7 @@ var option3=document.querySelector('#option3')
 var option4=document.querySelector('#option4')
 var timeLeft= 60
 var index= -1
+let time; 
 document.querySelector('#score').style='display:none'
 function nextQuestion(){
     index++
@@ -41,19 +42,38 @@ function nextQuestion(){
         document.querySelector('#quiz').style='display:none'
         document.querySelector('#score').style='display:block'
         document.querySelector('#userScore').textContent=initials+': '+timeLeft
+        localStorage.setItem('Highscore',initials+': '+timeLeft)
     }
-    questionEl.textContent=questions[index].question
-    option1.textContent=questions[index].option1
-    option2.textContent=questions[index].option2
-    option3.textContent=questions[index].option3
-    option4.textContent=questions[index].option4
+    else{
+        questionEl.textContent=questions[index].question
+        option1.textContent=questions[index].option1
+        option2.textContent=questions[index].option2
+        option3.textContent=questions[index].option3
+        option4.textContent=questions[index].option4
+    }
+  
+}
+function checkAnswer(){
+
 }
 option1.addEventListener('click',nextQuestion)
 option2.addEventListener('click',nextQuestion)
 option3.addEventListener('click',nextQuestion)
 option4.addEventListener('click',nextQuestion)
-nextQuestion()
-let time= setInterval(timer,1000)
+var startButton= document.getElementById ('startButton')
+var highScore =document.getElementById('highScore')
+startButton.addEventListener('click',function(){
+     time= setInterval(timer,1000)
+    nextQuestion()
+})
+highScore.addEventListener('click', function(){
+    var score =localStorage.getItem('Highscore')
+    var scoreList =document.getElementById('scoreList')
+    scoreList.textContent= score
+})
+
+
+
 function timer(){
    let timerEl=document.querySelector('#time') 
    timerEl.textContent=timeLeft
